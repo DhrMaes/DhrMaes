5f826b65307ba7f74061a8e6c72c6e25335d8f4e
DhrMaes <info@dhrmaes.com>
2021-01-20 20:48:09 +0100

    Webhook extension samen gesmeten

--- DhrMaesSolution/DhrMaesSolution/Program.cs
+++ DhrMaesSolution/DhrMaesSolution/Program.cs
@@ -9,7 +9,7 @@ namespace DhrMaesSolution {
     public class Program {
         public static void Main(string[] args) {
             Webhook hook = new Webhook("https://www.dhrmaes.com/CosmicCloud/Progress.php", "gRp33EXs3aNY4a2bePv9feev6qsUZPCdF44U2HPZWWK7TNJWqBndAxumkGVJ2QgHWZsKWDvYw6qCnP3xtkqBtzJUFnEDh2LdrUNrYsn9eWPZKW6zuVPA7DGxQg59UHuY");
-            Console.WriteLine(hook.HashHMAC_SHA256("{\"progress\":75}"));
+            Console.WriteLine(Webhook.GetPublicIP());
         }
     }
 }
--- DhrMaesSolution/NetPrograssTrackerExtension/Webhook.cs
+++ DhrMaesSolution/NetPrograssTrackerExtension/Webhook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -8,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NetPrograssTrackerExtension {
-    [NetPT.Progress(50), NetPT.ProgressCategory("SaveMethods"), NetPT.ProgressDescription("Creates a webhook so you can easily run it before building")]
+    [NetPT.Progress(100), NetPT.ProgressCategory("SaveMethods"), NetPT.ProgressDescription("Creates a webhook so you can easily run it before building")]
     public class Webhook {
         public string Endpoint { get; private set; }
         private string Secret = "";
@@ -37,9 +38,41 @@ namespace NetPrograssTrackerExtension {
             }
         }
 
+        public static string GetPublicIP() {
+            string url = "http://checkip.dyndns.org";
+            System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+            string response = sr.ReadToEnd().Trim();
+            string[] a = response.Split(':');
+            string a2 = a[1].Substring(1);
+            string[] a3 = a2.Split('<');
+            string a4 = a3[0];
+            return a4;
+        }
+
         private string HashHMAC_SHA256(string message) {
             var hash = new HMACSHA256(Secret.StringEncode());
             return hash.ComputeHash(message.StringEncode()).HashEncode();
+        }
+    }
+
+    public static class WebhookExtensions {
+        public static byte[] StringEncode(this string text) {
+            var encoding = new ASCIIEncoding();
+            return encoding.GetBytes(text);
+        }
+
+        public static string HashEncode(this byte[] hash) {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+
+        public static byte[] HexDecode(this string hex) {
+            var bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < bytes.Length; i++) {
+                bytes[i] = byte.Parse(hex.Substring(i * 2, 2), NumberStyles.HexNumber);
+            }
+            return bytes;
         }
     }
 }
