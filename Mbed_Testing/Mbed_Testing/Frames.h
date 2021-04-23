d65aa4025190e1c8c7c101af2a64dc6ce0c57f6b
DhrMaes <info@dhrmaes.com>
2021-04-23 20:43:10 +0200

    dunno

--- Mbed_Testing/Mbed_Testing/Frames.h
+++ Mbed_Testing/Mbed_Testing/Frames.h
@@ -7,13 +7,16 @@ private:
     unsigned char* CFrame;
 public:
     frame(int ip);
-    frame(unsigned char* RFrame, int ip);
+    frame(int ip, int length);
+    frame(unsigned char* oldFrame);
     void SetStartOfFrame();
+    void SetLength();
     void SetLength(int length);
     void SetId(int ip);
     void SetTemperature(float temperature);
     void SetPWM(int pwm);
     void SetTune(int tune);
+    void SetListIds(int ip);
     void SetListIds(unsigned char* RFrame, int ip);
     void SetChecksum();
     void SetEndOfFrame();
@@ -21,3 +24,5 @@ public:
     unsigned char* GetFrame();
     int GetLength();
 };
+
+static unsigned char CRC6(unsigned char* message, const int length);
--- Mbed_Testing/Mbed_Testing/Mbed_Testing.cpp
+++ Mbed_Testing/Mbed_Testing/Mbed_Testing.cpp
@@ -4,12 +4,63 @@
 #include <iostream>
 #include <string>
 #include "Frames.h"
+#include "Extensions.h"
+
 
 void print(unsigned char* list, int length);
+unsigned char MOD(unsigned char* list, int length, int mod);
 
 int main()
 {
+    unsigned char MODTest1[] = { 0x68, 0x01, 0x01 };
+    unsigned char MODResult1 = MOD(MODTest1, 3, 256);
+    std::cout << std::hex << MODResult1 << "\n";
+
+    //------------------------------------------------------
+
     std::cout << "Hello World!\n";
+
+    unsigned char crcTest1[] = { 0x0C, 0x65, 0x01, 0x08, 0xE6, 0x01, 0x69 };
+    //int crcResult1 = CRC6(crcTest1, 7);
+
+    //------------------------------------------------------
+
+    float ftosh = -22.250;
+    short shortTest = (short)(ftosh * 8);
+    std::cout << std::hex << shortTest << "\n";
+
+    //------------------------------------------------------
+
+    int mapTest = 168;
+    int mapResult = map(mapTest, 0, 255, 0, 100);
+    std::cout << mapResult << "\n";
+
+    //-------------------------------------------------------
+
+    unsigned char bitTestData1 = 0xB2;
+    unsigned char bitTestData2 = 0xF8;
+    //std::cout << std::hex << bitTestData1 << "\n";
+    //std::cout << std::hex << bitTestData2 << "\n";
+    signed short bitTestResult = bitTestData2 & 0x7;
+    bitTestResult <<= 8;
+    bitTestResult |= bitTestData1;
+    if (((bitTestData2 >> 7) & 0x1) == 1) {
+        bitTestResult = -bitTestResult;
+    }
+    float floatTest = bitTestResult;
+    floatTest = floatTest / 8;
+    std::cout << bitTestResult << "\n";
+
+    //-----------------------------------------------
+
+    int mapTest1 = map(0, 0, 255, 0, 100);
+    std::cout << mapTest1 << "\n";
+    int mapTest2 = map(255, 0, 255, 0, 100);
+    std::cout << mapTest2 << "\n";
+    int mapTest3 = map(100, 0, 255, 0, 100);
+    std::cout << mapTest3 << "\n";
+
+    //------------------------------------------------
 
     int length = 12;
     unsigned char* p;
@@ -28,14 +79,42 @@ int main()
     p = newp;
     print(p, nlength);
 
+    //----------------------------------------------------
+
+    std::cout << "FRAME TESTS" << "\n";
+    std::cout << "------------------------" << "\n";
+
+    //----------------------------------------------------
+
     frame* temp = new frame(111);
-    temp->SetPWM(100);
+    temp->SetStartOfFrame();
+    temp->SetLength();
+    temp->SetId(111);
     temp->SetTemperature(27);
+    temp->SetPWM(100);
     temp->SetTune(5);
+    temp->SetListIds(111);
     temp->SetChecksum();
     temp->SetEndOfFrame();
-    unsigned char* data = temp->GetFrame();
-    frame* newFrame = new frame(data, 112);
+    print(temp->GetFrame(), temp->GetLength());
+
+    frame* newFrame = new frame(112, (int)(temp->GetFrame()[2]) + 1);
+    //std::copy(temp->GetFrame(), temp->GetFrame() + temp->GetLength(), newFrame->GetFrame());
+    newFrame->SetListIds(temp->GetFrame(), 106);
+    newFrame->SetStartOfFrame();
+    newFrame->SetLength();
+    newFrame->SetId(106);
+    newFrame->SetTemperature(27);
+    newFrame->SetPWM(100);
+    newFrame->SetTune(5);
+    //newFrame->SetListIds(106);
+    newFrame->SetChecksum();
+    newFrame->SetEndOfFrame();
+    print(newFrame->GetFrame(), newFrame->GetLength());
+
+    frame* newFrame2 = new frame(newFrame->GetFrame());
+    print(newFrame2->GetFrame(), newFrame2->GetLength());
+
     float value = 27.10235113;
     int ivalue = (int)value;
     int tvalue = (int)(1000 * (value-ivalue));
@@ -47,9 +126,22 @@ int main()
 
 void print(unsigned char* list, int length) {
     for (int i = 0; i < length; i++) {
-        std::cout << (int)list[i] << "\n";
+        std::cout << std::to_string(i) << "\t" << (int)list[i] << "\n";
     }
     std::cout << "------------------------" << "\n";
+}
+
+static long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+unsigned char MOD(unsigned char* list, int length, int mod) {
+    int val = *list;
+    for (int i = 1; i < length; i++)
+        val += list[i];
+    unsigned char temp = ((65536 - val) % mod);
+    return temp;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
